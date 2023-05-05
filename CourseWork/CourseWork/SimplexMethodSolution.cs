e1bda6f0de040f72d2814a1771a8d11df01ae222
Sofya Kyzylova <121377923+SofyaKyzylova@users.noreply.github.com>
2023-05-05 22:03:06 +0500

    Update SimplexMethodSolution.cs
    gomory method implementation added. draft version

--- CourseWork/CourseWork/SimplexMethodSolution.cs
+++ CourseWork/CourseWork/SimplexMethodSolution.cs
@@ -101,20 +101,35 @@ namespace CourseWork
 
         List<List<double>> SimplexSolveF(int cols, int rows,
             List<double> functionValues, List<List<double>> limitValues, List<double> limitFreeValues, List<int> basicValuesIndexes,
-            double ResultFunctionF, int colsBeforeAdding, bool min)
+            double ResultFunctionF, bool min)
         {
             double resolvingElement;
             int colIndex;
             int rowIndex;
-            bool optimum = IsOptimum(functionValues, colsBeforeAdding);
+            bool optimum = IsOptimum(functionValues, cols);
+            bool negative = false;
+            int negIndex = -1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (limitFreeValues[i] < 0)
+                {
+                    optimum = false;
+                    negative = true;
+                    negIndex = i;
+                }
+            }
 
             while (!optimum)
             {
                 colIndex = functionValues.IndexOf(functionValues.Max());
 
-                rowIndex = FindRowIndex(limitValues, limitFreeValues, rows, colIndex);
+                if (negative)
+                    rowIndex = negIndex;
+                else rowIndex = FindRowIndex(limitValues, limitFreeValues, rows, colIndex);
                 if (rowIndex == -1)
                     return solution;
+                
 
                 resolvingElement = limitValues[rowIndex][colIndex];
                 basicValuesIndexes[rowIndex] = colIndex;
@@ -170,10 +185,10 @@ namespace CourseWork
                 solvingMatrix.Clear();
 
                 //проверяем план на оптимальность
-                optimum = IsOptimum(functionValues, colsBeforeAdding);
+                optimum = IsOptimum(functionValues, cols);
             }
 
-            return MakeSolution(colsBeforeAdding, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, min);
+            return MakeSolution(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, min);
         }
 
         List<List<double>> NoIOR(int cols, int rows)
@@ -282,9 +297,8 @@ namespace CourseWork
                     return solution = NoIOR(cols, rows);
             }
 
-            solution = SimplexSolveF(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, colsBeforeAdding, min);
-
-            return solution;
+            cols = colsBeforeAdding;
+            return SimplexSolveF(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, min);
         }
 
 
@@ -395,6 +409,140 @@ namespace CourseWork
             return SimplexFindIOR(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, functionT, ResultFunctionT, colsBeforeAdding, min);
         }
 
+
+        int MaxRowFractionIndex(List<List<double>> limitValues, List<double> fractions, int cols, int rows)
+        {
+            int maxFractionIndex = -1;
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if ((limitValues[i][j] - Math.Floor(limitValues[i][j]) != 0))
+                    {
+                        maxFractionIndex = i;
+                        break;
+                    }
+                }
+            }
+            
+            if (maxFractionIndex != -1 && maxFractionIndex != rows - 1)
+            {
+                for (int i = maxFractionIndex + 1; i < rows; i++)
+                {
+                    if (fractions[i] > fractions[maxFractionIndex])
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            if ((limitValues[i][j] - Math.Floor(limitValues[i][j]) != 0))
+                            {
+                                maxFractionIndex = i;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return maxFractionIndex;
+        }
+
+        List<double> GetFractions(int rows, List<double> limitFreeValues, List<double> fractions)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                if ((limitFreeValues[i] - Math.Floor(limitFreeValues[i]) != 0)) //проверяем есть ли дробные части
+                {
+                    fractions.Add(limitFreeValues[i] - Math.Floor(limitFreeValues[i]));
+                }
+                else
+                    fractions.Add(-1);
+            }
+            return fractions;
+        }
+
+        bool IsIntSolution(int rows, List<double> fractions)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                if (fractions[i] != -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        List<List<double>> GomoryMethod(int cols, int rows, List<double> functionValues, List<List<double>> limitValues,
+            List<double> limitFreeValues, List<int> basicValuesIndexes, double ResultFunctionF, bool min)
+        {
+            List<double> fractions = new List<double>();
+            fractions = GetFractions(rows, limitFreeValues, fractions);
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    if ((limitFreeValues[i] - Math.Floor(limitFreeValues[i]) != 0)) //проверяем есть ли дробные части
+            //    {
+            //        fractions.Add(limitFreeValues[i] - Math.Floor(limitFreeValues[i]));
+            //    }
+            //    else
+            //        fractions.Add(-1);
+            //}
+
+            //bool flag = false;
+            //for(int i = 0; i < rows; i++)
+            //{
+            //    if (fractions[i] != -1)
+            //    {
+            //        flag = true;
+            //        break;
+            //    }
+            //}
+
+            bool flag = IsIntSolution(rows, fractions);
+
+            if (!flag) //ответ целочисленный, РЕШЕНИЕ НЕ НУЖНО
+            {
+                return NoIOR(cols, rows);
+            }
+
+
+            while (flag) 
+            {
+                int maxFraction = MaxRowFractionIndex(limitValues, fractions, cols, rows);
+                if (maxFraction == -1)
+                {
+                    List<List<double>> empty = new List<List<double>>();
+                    return empty;  //если не нашлось ни одной строки с дробными значениями - ЦЕЛОЧИСЛЕННОГО РЕШЕНИЯ НЕТ
+                }
+
+                List<double> rowFractions = new List<double>();
+                for (int j = 0; j < cols; j++)
+                {
+                    rowFractions.Add(limitValues[maxFraction][j] - Math.Floor(limitValues[maxFraction][j])); //дробные части чисел найденной строки
+                    rowFractions[j] *= -1;
+                }
+                rowFractions.Add(1);
+
+                //составим доп ограничение:
+                for (int i = 0; i < rows; i++)
+                {
+                    limitValues[i].Add(0);
+                }
+
+                cols++;
+                rows++;
+                limitFreeValues.Add((limitFreeValues[maxFraction] - Math.Floor(limitFreeValues[maxFraction])) * (-1)); //дробная часть свободного члена
+                limitValues.Add(rowFractions);
+                functionValues.Add(0);
+                basicValuesIndexes.Add(cols);
+
+                solution = SimplexSolveF(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, min);
+
+                fractions.Clear();
+                fractions = GetFractions(rows, limitFreeValues, fractions);
+                flag = IsIntSolution(rows, fractions);
+            }
+
+            return solution;
+        }
 
 
         internal SimplexMethodSolution(int cols, int rows, List<double> functionValues, List<List<double>> limitValues, 
@@ -581,18 +729,147 @@ namespace CourseWork
                         Font = new Font("Microsoft Sans Serif", 11),
                         Text = "Найти целочисленное решение"
                     };
-                    this.Controls.Add(intSolutionButton);
-
-
-                    intSolutionButton.Click += (sender, eventArgs) =>
-                    { 
-                    
-                    };
-
                     this.Controls.Add(richTextBox1);
                     this.Controls.Add(DGV);
                     this.Controls.Add(intSolutionButton);
 
+
+                    intSolutionButton.Click += (sender, eventArgs) =>
+                    {
+                        RichTextBox richTextBox2 = new RichTextBox
+                        {
+                            Location = new Point(10, richTextBox1.Height + dgv_height + 320),
+                            Font = new Font("Microsoft Sans Serif", 11),
+                            ReadOnly = true
+                        };
+                        richTextBox2.ContentsResized += (object senderRichTextBox, ContentsResizedEventArgs e) =>
+                        {
+                            var richTextBox = (RichTextBox)senderRichTextBox;
+                            richTextBox.Height = e.NewRectangle.Height;
+                        };
+                        richTextBox2.WordWrap = false;
+                        richTextBox2.ScrollBars = RichTextBoxScrollBars.None;
+
+
+                        functionValues.Clear();
+                        limitValues.Clear();
+                        limitFreeValues.Clear();
+
+                        List<int> basicValuesIndexes = new List<int>();
+                        double ResultFunctionF = solution[rows - 1][1];
+
+                        for(int i = 0; i < rows; i++)
+                        {
+                            List<double> limits = new List<double>();
+                            for (int j = 0; j < cols; j++)
+                            {
+                                if (j == 0 && i != rows - 1)
+                                {
+                                    basicValuesIndexes.Add(Convert.ToInt32(solution[i][j]) - 1);
+                                }
+                                else if (j == 1 && i != rows - 1)
+                                {
+                                    limitFreeValues.Add(solution[i][j]);
+                                }
+                                else if (j > 1 && i != rows - 1)
+                                {
+                                    limits.Add(solution[i][j]);
+                                }
+                                else
+                                {
+                                    functionValues.Add(solution[i][j]);
+                                }
+                            }
+                            limitValues.Add(limits);
+                        }
+
+                        solution.Clear();
+                        solution = GomoryMethod(cols, rows, functionValues, limitValues, limitFreeValues, basicValuesIndexes, ResultFunctionF, min);
+
+                        if (!solution.Any())
+                        {
+                            richTextBox2.Text += "Задача не имеет целочисленных решений. \r\n";
+                            richTextBox2.Width = 450;
+                            this.Controls.Add(richTextBox2);
+                        }
+                        else
+                        {
+                            richTextBox2.Text += "Решение задачи методом Гомори: нахождение целочисленного решения. \r\n";
+                            cols = solution[0].Count();
+                            rows = solution.Count();
+                            bool intFlag = true;
+
+                            for (int i = 0; i < rows; i++)
+                            {
+                                for (int j = 0; j < cols; j++)
+                                {
+                                    if (solution[i][j] != 0)
+                                    {
+                                        intFlag = false;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (intFlag)
+                            {
+                                richTextBox2.Text += "\r\n";
+                                richTextBox2.Text += "Полученный ответ является целочисленным. Нет необходимости применять метод Гомори. \r\n";
+                                richTextBox2.Width = 450;
+                                this.Controls.Add(richTextBox2);
+                            }
+                            else
+                            {
+                                DataGridView DGV2 = new DataGridView
+                                {
+                                    Font = new Font("Microsoft Sans Serif", 11),
+                                    Location = new Point(10, richTextBox1.Height + dgv_height + richTextBox2.Height + 140),
+                                    AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells,
+                                    AutoSize = true,
+                                    ReadOnly = true,
+
+                                    ColumnCount = rows,
+                                    RowCount = cols
+                                };
+
+                                DGV2.Columns[0].HeaderText = "Базис";
+                                DGV2.Columns[1].HeaderText = "Св. чл.";
+                                int k2 = 1;
+                                for (int i = 2; i < rows; i++)
+                                {
+                                    DGV2.Columns[i].HeaderText = "X" + k2.ToString();
+                                    k2++;
+
+                                }
+
+                                for (int i = 0; i < cols; i++)
+                                {
+                                    for (int j = 0; j < rows; j++)
+                                    {
+                                        if (i < cols - 1 && j == 0)
+                                        {
+                                            DGV2.Rows[i].Cells[j].Value = "X" + Math.Round(solution[j][i], 3).ToString();
+                                        }
+                                        else if (i == cols - 1 && j == 0)
+                                        {
+                                            DGV2.Rows[i].Cells[j].Value = "F";
+                                        }
+                                        else
+                                        {
+                                            DGV2.Rows[i].Cells[j].Value = Math.Round(solution[j][i], 2).ToString();
+                                        }
+                                    }
+                                }
+
+                                int dgvWidth = DGV2.Columns.GetColumnsWidth(DataGridViewElementStates.Visible);
+                                int dgvHeight = DGV2.Rows.GetRowsHeight(DataGridViewElementStates.Visible);
+                                richTextBox2.Width = dgvWidth / 2 + 100;
+
+                                this.Controls.Add(richTextBox2);
+                                this.Controls.Add(DGV2);
+                            };
+                        }
+                    };
                 }                
             }
         }
